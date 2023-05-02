9f279b651b581fe2538fb5aef6d82c2ad00e229a
hussainkeshwani61 <your_email_address@example.com>
2023-05-02 10:57:30 +0100

    Setup Database for financeportal

---
+++ database/migrations/2023_04_15_172347_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string("invoice_ref");
+            $table->string("student_id");
+            $table->string("amount");
+            $table->string("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
