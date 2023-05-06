6ee13c54fb128a0ea28d485ccb30c9fe3385c871
hussainkeshwani61 <your_email_address@example.com>
2023-05-06 12:24:12 +0100

    Done

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -7,18 +7,19 @@ use App\Models\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
-    public function find_invoice(Request $request){
+    public function find_invoice(Request $request)
+    {
         $invoice_ref = $request->input('invoice_ref');
         $invoice = Invoice::where('invoice_ref', $invoice_ref)->first();
         if($invoice){
             return view('invoice', ['invoice' => $invoice]);
         }else{
-            return view('index', ['error' => 'Invoice not found']);
+            return view('index', ['error' => 'Sorry!, Invoice does not exits.']);
         }
     }
 
-    public function pay_invoice(Request $request){
+    public function pay_invoice(Request $request)
+    {
         $invoice_ref = $request->input('invoice_ref');
         $invoice = Invoice::where('invoice_ref', $invoice_ref)->first();
         if($invoice){
@@ -26,14 +27,12 @@ class InvoiceController extends Controller
             $invoice->save();
             return view('invoice', ['invoice' => $invoice]);
         }else{
-            return view('index', ['error' => 'Invoice not found']);
+            return view('index', ['error' => 'Sorry!, Invoice does not exits.']);
         }
     }
 
-    //generate invoice function for API data coming in params
     public function generate_invoice($student_id, $amount){
         $invoice = new Invoice;
-        //generate invoice reference of 10 characters
         $invoice_ref = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(10/strlen($x)) )),1,10);
         $invoice->invoice_ref = $invoice_ref;
         $invoice->student_id = $student_id;
@@ -41,25 +40,15 @@ class InvoiceController extends Controller
         $invoice->status = "UNPAID";
         $invoice->save();
 
-        return response()->json([
-            'message' => 'Invoice generated successfully',
-            'invoice' => $invoice
-        ], 201);
+        return response()->json(['message' => 'Invoice generated successfully','invoice' => $invoice], 201);
     }
 
-    //check invoices function for API data coming in params
     public function check_invoices($student_id){
         $invoices = Invoice::where('student_id', $student_id)->get();
         if($invoices){
-            return response()->json([
-                'message' => 'Invoices found',
-                'invoices' => $invoices
-            ], 200);
+            return response()->json(['message' => 'Invoices found','invoices' => $invoices], 200);
         }else{
-            return response()->json([
-                'message' => 'No invoices found',
-                'invoices' => $invoices
-            ], 404);
+            return response()->json(['message' => 'Sorry!, Invoice does not exits.','invoices' => $invoices], 404);
         }
     }
 }
